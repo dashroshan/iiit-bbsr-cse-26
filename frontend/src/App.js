29aeb896d393d4df096952f4d3b8379a4c873f08
Roshan Dash <roshan1337d@gmail.com>
2022-11-05 17:20:12 +0530

    loading screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes, useLocation, Outlet } from 'react-router-dom';
 import { motion } from "framer-motion";
+import { useEffect } from 'react';
 
 import HomePage from './routes/homePage';
 import Experimental from './routes/experimental';
@@ -46,6 +47,21 @@ const AnimationLayout = () => {
 };
 
 export default function App() {
+    useEffect(() => {
+        const onPageLoad = () => {
+            document.getElementById("loader_block").style.opacity = 0;
+            setTimeout(() => {
+                document.getElementById("loader_block").style.display = "none";
+            }, 305);
+        };
+        if (document.readyState === 'complete') {
+            onPageLoad();
+        } else {
+            window.addEventListener('load', onPageLoad, false);
+            return () => window.removeEventListener('load', onPageLoad);
+        }
+    }, []);
+
     return (
         <div className="App">
             <TopBinary />
